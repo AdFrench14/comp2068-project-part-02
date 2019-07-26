3db1bd2d2b9f7f7a3f405c3816b3285b9c5f017c
Adrien French <ad.french14@gmail.com>
2019-07-26 10:27:35 -0400

    updated

---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatWindow() {
+  return (
+      <div>
+          <p>Testing Chat Window Element</p>
+      </div>
+      );
+}
+
+export default ChatWindow;
---
+++ client/src/components/MainNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainNav() {
+  return (
+      <div>
+          <p>Testing Main Nav Element</p>
+      </div>
+  );
+}
+
+export default MainNav;
